e53c5bdf0ab4b6e5fdf486e84a01a5153bcfde05
Amine Neggazi <neggazimedlamine@gmail.com>
2022-08-18 18:15:48 +0100

    added shipment state and id styling for grid cards!

--- pages/components/home/Main.js
+++ pages/components/home/Main.js
@@ -9,6 +9,7 @@ import {
   CardContent,
   CardHeader,
   CardMedia,
+  Chip,
   Collapse,
   Divider,
   Modal,
@@ -43,65 +44,65 @@ const Item = styled(Paper)(({ theme }) => ({
 const shipments = [
   {
     'id': 'qaf-2nbd231k',
-    'name': 'alilo chekima',
+    'name': 'Amine Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'New',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n1d231k',
-    'name': 'alilo chekima',
+    'name': 'Ahmed Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'Done',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n2d231k',
-    'name': 'alilo chekima',
+    'name': 'Sofian Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'Ready',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n3d231k',
-    'name': 'alilo chekima',
+    'name': 'Dahmane Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'Done',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n4d231k',
-    'name': 'alilo chekima',
+    'name': 'Lamia Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'New',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n5d231k',
-    'name': 'alilo chekima',
+    'name': 'Rania Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'Ready',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n6d231k',
-    'name': 'alilo chekima',
+    'name': 'Soumia Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'Done',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n7d231k',
-    'name': 'alilo chekima',
+    'name': 'Hassen Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'New',
     'date': 'xx-xx-xxxx',
   },
   {
     'id': 'qaf-2n8d231k',
-    'name': 'alilo chekima',
+    'name': 'Yacine Chekima',
     'address': 'Sidi belabbes',
-    'state': 'new shipment',
+    'state': 'New',
     'date': 'xx-xx-xxxx',
   },
 ]
@@ -139,7 +140,7 @@ export default function Main() {
           }
         }}
         size='large'
-        variant="contained"
+        variant='contained'
         startIcon={<AddIcon />}
         onClick={handleOpen}
       >
@@ -149,8 +150,8 @@ export default function Main() {
         keepMounted
         open={open}
         onClose={handleClose}
-        aria-labelledby="keep-mounted-modal-title"
-        aria-describedby="keep-mounted-modal-description"
+        aria-labelledby='keep-mounted-modal-title'
+        aria-describedby='keep-mounted-modal-description'
       >
         <Box sx={style}>
           <Stack direction='column' spacing={2} height='100%' justifyContent='space-between'>
@@ -164,7 +165,7 @@ export default function Main() {
               <Divider />
             </Stack>
             <Stack direction='column' spacing={2} height='100%' justifyContent='space-between'>
-              <Alert severity="warning">
+              <Alert severity='warning'>
                 <Typography fontSize={10}>
                   Note: You must have an email address
                 </Typography>
@@ -227,37 +228,35 @@ export default function Main() {
           width: '97%',
         }}
       >
-        <Grid container pr={{ xs: 0, sm: 1, md: 0 }} spacing={{ xs: 6, sm: 4, md: 2 }} columns={{ xs: 1, sm: 8, md: 16 }}>
+        <Grid container pr={{ xs: 0, sm: 1, md: 0 }} spacing={{ xs: 6, sm: 4, md: 2 }} columns={{ xs: 1, sm: 8, md: 12 }}>
           {shipments.map((item, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
               <Card>
                 <CardHeader
                   avatar={
-                    <Avatar sx={{ bgcolor: colors[Math.floor(Math.random()*colors.length)] }} aria-label="recipe">
-                      R
+                    <Avatar sx={{ bgcolor: colors[Math.floor(Math.random()*colors.length)] }} aria-label='recipe'>
+                      { item.name.charAt(0).toUpperCase() }
                     </Avatar>
                   }
                   action={
-                    <IconButton aria-label="settings">
+                    <IconButton aria-label='settings'>
                       <MoreVertIcon />
                     </IconButton>
                   }
-                  title="Shrimp and Chorizo Paella"
-                  subheader="September 14, 2016"
+                  title={ item.name }
+                  subheader={ item.address }
                 />
-                <CardContent>
-                  <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish.
-                  </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                  <IconButton aria-label="share">
-                    <ShareIcon />
-                  </IconButton>
-                </CardActions>
+                <Stack
+                  spacing={2}
+                  direction='row'
+                  sx={{
+                    m: 1,
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Chip label={ item.id } variant='outlined' />
+                  <Chip label={ item.state } variant='contained' />
+                </Stack>
               </Card>
             </Grid>
           ))}
