3ce3d649a5482ba57e8d489c10eb82deee494495
Amine Neggazi <neggazimedlamine@gmail.com>
2022-08-18 13:52:52 +0100

    added add shipment button and modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import Layout from './components/layouts/Layout'
+import '@fontsource/roboto/500.css'
 
 
 export default function MyApp({ Component, pageProps }) {
--- pages/components/home/Main.js
+++ pages/components/home/Main.js
@@ -4,12 +4,18 @@ import {
   Alert,
   IconButton,
   Collapse,
+  Divider,
   Button,
   Paper,
-  Grid
+  Grid,
+  Stack,
+  Modal,
+  Typography,
 } from '@mui/material'
-import { experimentalStyled as styled } from '@mui/material/styles';
+import { experimentalStyled as styled } from '@mui/material/styles'
 import CloseIcon from '@mui/icons-material/Close'
+import AddIcon from '@mui/icons-material/Add'
+import CheckIcon from '@mui/icons-material/Check'
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -86,12 +92,105 @@ const shipments = [
   },
 ]
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '70%',
+  height: '70%',
+  bgcolor: 'white',
+  border: '2px solid #EEEEEE',
+  borderRadius: 2,
+  boxShadow: 24,
+  p: 4,
+};
+
 
 export default function Main() {
-  const [open, setOpen] = React.useState(true)
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
+      <Button 
+        sx={{
+          width: { xs: '90%', sm: '95%', md: 'auto' },
+          ml: 2,
+          mt: 4,
+          backgroundColor: '#7D749E',
+          '&:hover': {
+            backgroundColor: '#884FAA'
+          }
+        }}
+        size='large'
+        variant="contained"
+        startIcon={<AddIcon />}
+        onClick={handleOpen}
+      >
+        Add shipment
+      </Button>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+          <Stack direction='column' spacing={2} height='100%' justifyContent='space-between'>
+            <Stack direction='column' spacing={2}>
+              <Stack direction='row' spacing={1}>
+                <AddIcon /> 
+                <Typography>
+                  Add Shipment
+                </Typography>
+              </Stack>
+              <Divider />
+            </Stack>
+            <Alert severity="warning">
+              <Typography fontSize={9}>
+                Note: You must have an email address
+              </Typography>
+            </Alert>
+            <Stack direction='column' spacing={2}>
+              <Divider />
+              <Stack direction='row' spacing={2} height={40}>
+                <Button 
+                  sx={{
+                    backgroundColor: '#7D749E',
+                    '&:hover': {
+                      backgroundColor: '#884FAA'
+                    }
+                  }}
+                  variant='contained'
+                  startIcon={<CheckIcon />}
+                >
+                  Confirm
+                </Button>
+                <Button 
+                  sx={{
+                    color: '#7D749E',
+                    borderColor: '#7D749E',
+                    backgroundColor: '#FFFFFF',
+                    '&:hover': {
+                      color: '#7D749E',
+                      borderColor: '#FFFFFF',
+                      backgroundColor: '#DDDDDD',
+                    }
+                  }}
+                  variant='outlined'
+                  startIcon={<CloseIcon />}
+                  onClick={handleClose}
+                >
+                  Cancel
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Box>
+      </Modal>
       <Box sx={{ 
           mt: {xs: 4, sm: 8, md: 10},
           mx: 'auto',
