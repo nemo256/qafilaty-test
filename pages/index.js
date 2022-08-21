ad0a39700002bda7b1ad2e841300a2dc9ed03566
Amine Neggazi <neggazimedlamine@gmail.com>
2022-08-21 13:52:17 +0100

    added apollo client and schema

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 import Main from './components/home/Main'
 
 
 export default function Home() {
+  const client = new ApolloClient({
+    uri: 'https://test-api-9kapg.qafilaty.com/graphql',
+    cache: new InMemoryCache(),
+  });
+
   return (
     <Main />
   )
